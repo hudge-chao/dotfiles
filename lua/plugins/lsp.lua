8a507ad4224f0876efdca376648ee15dd2d31fac
hudge-chao <987937104@qq.com>
2026-01-18 23:13:56 +0800

    add mason ui

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,13 +1,32 @@
-return {
-  {
-    "mason-org/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "vetur-vls",
-      },
-    },
-  },
-}
+-- return {
+--   {
+--     "mason-org/mason.nvim",
+--     opts = {
+--       ensure_installed = {
+--         "vetur-vls",
+--       },
+--     },
+--   },
+-- }
+
+-- {
+--   "neovim/nvim-lspconfig",
+--   opts = {
+--     servers = {
+--       -- 这里配置 volar
+--       volar = {
+--         init_options = {
+--           vue = {
+--             -- 对于 Vue 2，建议关闭混合模式以提高兼容性
+--             hybridMode = false,
+--           },
+--         },
+--         -- 如果你需要指定特定的文件类型
+--         filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue", "json" },
+--       },
+--     },
+--   },
+-- },
 
 -- return {
 --   {
---
+++ lua/plugins/mason.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "mason-org/mason.nvim",
+    opts = {
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗",
+        },
+        border = "rounded",
+        width = 0.8,
+        height = 0.9,
+      },
+      max_concurrent_installers = 4,
+      PATH = "prepend",
+    },
+  },
+}
